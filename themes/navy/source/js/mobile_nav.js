8d885ac5ae13bf621f10fbb5a232ef921fdbdd52
Nistor Cristian <contact@nistorcristian.com>
2018-10-04 15:50:01 +0300

    Improved responsve for docs. Fixed images on homepage. Created submenu. Improved responsive on homepage.

--- themes/navy/source/js/mobile_nav.js
+++ themes/navy/source/js/mobile_nav.js
@@ -19,8 +19,27 @@ $(document).ready(function () {
 
   setMenu(w);
 
-  $('.mobile-menu-trigger, .backdrop, .mobile-menu-container .close').on('click', function () {
+  $('.mobile-menu-trigger, .backdrop, .mobile-menu-container .close').on('click', function (event) {
+    event.preventDefault();
     $('body').toggleClass('open');
+    $('body').removeClass('open-submenu');
+  });
+
+  $('.mobile-menu-container .mobile-submenu-container-inner .mobile-menu-header .back').on('click', function (event) {
+    event.preventDefault();
+    $('body').removeClass('open-submenu');
+  });
+
+  $('.mobile-menu-container-inner .has-submenu .main-link').each(function (index, element) {
+    $(this).on('click', function (event) {
+      event.preventDefault();
+      var cloneSubMenu = $(this).parent().find('.sub-menu ul').clone(),
+          subMenuTitle = $(this).text();
+      $('.mobile-menu-container .mobile-submenu-container-inner .dropdown nav').empty();
+      $('body').addClass('open-submenu');
+      cloneSubMenu.appendTo('.mobile-menu-container .mobile-submenu-container-inner .dropdown nav');
+      $('.mobile-menu-container .mobile-submenu-container-inner .title').text(subMenuTitle);
+    });
   });
   
 });
