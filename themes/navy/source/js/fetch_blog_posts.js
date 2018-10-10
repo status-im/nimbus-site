0d3ce9f59d66a6f1618b0d30f95d09c45ba97ccb
Nistor Cristian <contact@nistorcristian.com>
2018-10-10 12:22:43 +0300

    Improved blog posts.

--- themes/navy/source/js/fetch_blog_posts.js
+++ themes/navy/source/js/fetch_blog_posts.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
-  let url = 'https://our-status.ghost.io/ghost/api/v0.1/posts/?limit=3&formats=plaintext&client_id=ghost-frontend&client_secret=1c5590b47eb4';
+  var url = 'https://our-status.ghost.io/ghost/api/v0.1/posts/?limit=3&include=tags&filter=tags:nimbus&formats=plaintext&client_id=ghost-frontend&client_secret=1c5590b47eb4';
+  var urlBase = [location.protocol, '//', location.host, location.pathname].join('');
   $.ajax({
     type: "get",
     url: url,
     success: function (response) {
       $.each(response.posts, function (index, val) { 
-        $('.contribute.blog:nth-child('+ (index+1) +') h3').text(val.title);
+        var excerpt = '';
         if(val.custom_excerpt != null) {
-          $('.contribute.blog:nth-child('+ (index+1) +') p').text(val.custom_excerpt);
+          excerpt = val.custom_excerpt;
         }else{
-          $('.contribute.blog:nth-child('+ (index+1) +') p').text(getWords(val.plaintext));
+          excerpt = getWords(val.plaintext);
         }
+        $('.contribute-wrap-blog').prepend('<div class="contribute blog"><a href="https://our.status.im/'+ val.slug +'" class="feature-image"><img src="https://our.status.im/'+ val.feature_image +'" alt="'+ val.title +'"></a><h3><a href="https://our.status.im/'+ val.slug +'">'+ val.title +'</a></h3><p>'+ excerpt +'</p><a href="https://our.status.im/'+ val.slug +'">Read More <img src="'+ urlBase +'/img/arrow_ogn.png" /></a></div>');
       });
     }
   });
